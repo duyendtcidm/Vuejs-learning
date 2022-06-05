94434a55af029ecd32b3c4432c459e4de7d1bcad
duyendtcidm <duyendtcidm@gmail.com>
2022-06-05 08:35:19 +0700

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      names: ['Bruce', 'Clark', 'Diana']
+    }
+  },
 }
 </script>
 
@@ -23,4 +22,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
