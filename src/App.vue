afa289bc0702b9e70dfbe227adc9599770f0d95c
duyendtcidm <duyendtcidm@gmail.com>
2022-06-05 13:56:31 +0700

    method

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
-  <div v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name}}</h2>
-    <h2 v-for="movie in actor.movies" :key="movie">{{ movie }}</h2>
-  </div>
-
-  <h2 v-for="(info, key, index) in myInfo" :key="info">{{ index }} : {{ key }} {{ info }}</h2>
+<!-- Can't use arrow function in method & I don't know why-->
+  <h2> Add method: {{ add(2, 3, 4)}}</h2>
+  <h2> Suctract method: {{ subtract(3, 4)}}</h2>
+  <h2> Multiply by {{ multyply(23)}}</h2>
 </template>
 
 <script>
@@ -14,29 +10,21 @@ export default {
   name: 'App',
   data() {
     return {
-      names: ['Bruce', 'Clark', 'Diana'],
-      fullNames: [
-        { first: 'Iris', last: 'DeLora'},
-        { first: 'Anita', last: 'Duong'},
-        { first: 'Jess', last: 'Tham'},
-      ],
-      actors: [
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'American Psycho'],
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titanic', 'Inception'],
-        },
-      ],
-      myInfo: {
-        name: 'Iris DeLora',
-        course: 'Vue 3',
-        age: 22
-      },
-    }
+      baseMultyply: 5,
+    } 
   },
+
+  methods: {
+      add(a, b, c) {
+        return a+b+c
+      },
+      subtract(a, b) {
+        return a-b
+      },
+      multyply(num) {
+        return num * this.baseMultyply
+      }
+    }
 }
 </script>
 
