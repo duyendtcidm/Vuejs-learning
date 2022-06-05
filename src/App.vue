5773de6398ddb2f50c78410d295b8752291c9812
duyendtcidm <duyendtcidm@gmail.com>
2022-06-05 09:08:43 +0700

    v-for done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{ actor.name}}</h2>
+    <h2 v-for="movie in actor.movies" :key="movie">{{ movie }}</h2>
+  </div>
+
+  <h2 v-for="(info, key, index) in myInfo" :key="info">{{ index }} : {{ key }} {{ info }}</h2>
 </template>
 
 <script>
@@ -7,7 +14,27 @@ export default {
   name: 'App',
   data() {
     return {
-      names: ['Bruce', 'Clark', 'Diana']
+      names: ['Bruce', 'Clark', 'Diana'],
+      fullNames: [
+        { first: 'Iris', last: 'DeLora'},
+        { first: 'Anita', last: 'Duong'},
+        { first: 'Jess', last: 'Tham'},
+      ],
+      actors: [
+        {
+          name: 'Christian Bale',
+          movies: ['Batman', 'American Psycho'],
+        },
+        {
+          name: 'Di Caprio',
+          movies: ['Titanic', 'Inception'],
+        },
+      ],
+      myInfo: {
+        name: 'Iris DeLora',
+        course: 'Vue 3',
+        age: 22
+      },
     }
   },
 }
